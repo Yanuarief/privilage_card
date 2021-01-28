719f844e3025dbfb293274156be3005b5e0a6e57
Anjay <jayang.sp@gmail.com>
2021-01-28 09:11:00 +0700

    Clear All Without DB

--- admin/controllers/html-entities/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  XmlEntities: require('./lib/xml-entities.js'),
-  Html4Entities: require('./lib/html4-entities.js'),
-  Html5Entities: require('./lib/html5-entities.js'),
-  AllHtmlEntities: require('./lib/html5-entities.js')
-};
